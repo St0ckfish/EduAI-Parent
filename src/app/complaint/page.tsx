83e5c5c4e71f456878e031d24e9864d4d337148b
M0staphaTaha <darsh.taha12382@gmail.com>
2024-12-22 13:35:59 +0200

    notifications

--- src/app/complaint/page.tsx
+++ src/app/complaint/page.tsx
@@ -8,7 +8,7 @@ import {
   useCreateComplaint,
   useGetAllComplains,
 } from "~/APIs/hooks/useComplains";
-import { ComplaintResponse, Student } from "~/types";
+import { type ComplaintResponse, type Student } from "~/types";
 import { useGetStudents, useGetStudentTeachers } from "~/APIs/hooks/useStudent";
 import { useState } from "react";
 import { toast } from "react-toastify";
