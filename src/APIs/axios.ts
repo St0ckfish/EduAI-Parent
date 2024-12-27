493d60e43fbf3d06f0826cb6b9d14a457d453943
M0staphaTaha <darsh.taha12382@gmail.com>
2024-12-27 13:18:39 +0200

    fix bug

--- src/APIs/axios.ts
+++ src/APIs/axios.ts
@@ -1,9 +1,14 @@
 import axios from "axios";
 import Cookies from "js-cookie";
+export const baseUrlStock = `wss://api.eduai.tech/`; //wss://eduai.vitaparapharma.com //wss://195.35.28.106:8091 //wss://api.eduai.tech
 
 const axiosInstance = axios.create({
+<<<<<<< HEAD
   baseURL: "https://eduai.vitaparapharma.com",
   // baseURL: "https://api.eduai.tech",
+=======
+  baseURL: "https://api.eduai.tech/",
+>>>>>>> 074831f (fix bug)
 });
 
 axiosInstance.interceptors.request.use(
