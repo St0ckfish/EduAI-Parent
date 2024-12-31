e5d12673e50ee617ab90facf10db13740537b20e
M0staphaTaha <darsh.taha12382@gmail.com>
2024-12-31 19:05:46 +0200

    fix bug

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -93,6 +93,10 @@ const Login = () => {
                   </p>
                 )}
               </label>
+              <div className="flex gap-2 items-center">
+              Remember Me
+                <input type="checkbox" name="remember" id="" />
+              </div>
               <div className="flex justify-end text-end">
                 <a href="/forget-password">
                   <Text font={"medium"} size={"md"} color={"muted"}>
